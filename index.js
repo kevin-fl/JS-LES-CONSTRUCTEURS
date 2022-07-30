48e79f6a5f2a7aede4e47450594339b11970b3dd
kevin-fl <kevinflamang@gmail.com>
2022-07-30 13:02:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// CREE UN OBJET EN JS 
+
+
+
+// 1ere maniere avec ajout de proprietes a l objet initial (age , profession)
+
+const monObjet = {
+    nom: "kevin"
+
+};
+
+monObjet.age = 34;
+monObjet.profession = "web developer"
+
+
+console.log(monObjet);
+
+
